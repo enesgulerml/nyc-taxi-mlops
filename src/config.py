95d3499e21003a9c10a2d963c373a9598abcdce5
Enes Güler <enesguler.ml@gmail.com>
2026-01-25 20:17:02 +0300

    chore: add sample data and configure dynamic path for CI

--- src/config.py
+++ src/config.py
@@ -3,8 +3,14 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
+# --- CI/CD & DATA SELECTION LOGIC ---
+if os.getenv("CI") == "true":
+    DATA_FILENAME = "sample_data.csv"
+else:
+    DATA_FILENAME = "train.csv"
+
 # DATA PATHS
-DATA_RAW_PATH = os.path.join(ROOT_DIR, "data", "raw", "train.csv")
+DATA_RAW_PATH = os.path.join(ROOT_DIR, "data", "raw", DATA_FILENAME)
 MODEL_SAVE_PATH = os.path.join(ROOT_DIR, "models", "nyc_taxi_model.onnx")
 LOG_FILE_PATH = os.path.join(ROOT_DIR, "logs", "running_logs.log")
 
